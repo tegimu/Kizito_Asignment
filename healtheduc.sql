f17fe60b5be66151b9d807810a907bd7d9587174
madrine nalukwago <mnalukwago26@gmail.com>
2025-09-16 16:53:09 +0300

    Add Fields for Geo-location

--- healtheduc.sql
+++ healtheduc.sql
@@ -6,7 +6,15 @@ doc::jsonb->>'form'as form,
 doc::jsonb->>'from'as from,
 doc::jsonb->>'type'as type,
 doc::jsonb->'fields'->'meta'->>'instanceID'as instanceID,
-doc::jsonb->'fields'->'meta'->>'deprecatedID'as deprecatedID
+doc::jsonb->'fields'->'meta'->>'deprecatedID'as deprecatedID,
+doc::jsonb->'geolocation_log'->'recording'->>'speed' AS geolocation_log_speed,
+doc::jsonb->'geolocation_log'->'recording'->>'heading' AS geolocation_log_heading, 	doc::jsonb->'geolocation_log'->'recording'->>'accuracy' AS geolocation_log_accuracy,
+(doc::jsonb->'geolocation_log'->'recording'->>'altitude')::decimal AS geolocation_log_altitude,
+(doc::jsonb->'geolocation_log'->'recording'->>'latitude'):: decimal AS geolocation_log_latitude,
+(doc::jsonb->'geolocation_log'->'recording'->>'longitude'):: decimal AS geolocation_log_longitude,
+doc::jsonb->'geolocation_log'->'recording'->>'altitudeAccuracy' AS altitudeAccuracy,
+doc::jsonb->'geolocation_log'->>'timestamp' AS timestamp
+ 	
 FROM "Reports".death_notification
 WHERE type = 'data_record'
 AND TRIM(doc) LIKE '{%' -- Ensures it starts with an opening curly brace after trimming whitespace
